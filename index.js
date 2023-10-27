910fe0af369807bdba5454caf229f4382813beaa
Atharva <atharvajoshi202@gmail.com>
2023-10-28 00:40:38 +0530

    Add recording and song saving functionalities
    - Introduce a "Record" button to allow users to record drum beats.
    - Implement the ability to save recorded songs.
    - Implement the ability to play songs.
    - Enhance user interaction by providing a visual cue for recording mode.
    - Ensure that saved songs are properly stored in local storage.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+let recordingStartTime;
+let songNotes;
 
+displayAllKeysInDiv();
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
@@ -17,6 +20,9 @@ document.addEventListener("keypress", function (event) {
 });
 
 function makeSound(key) {
+  if (isRecording()) {
+    recordNote(key);
+  }
   switch (key) {
     case "w":
       var tom1 = new Audio("sounds/tom-1.mp3");
@@ -68,9 +74,149 @@ function buttonAnimation(currentKey) {
   }, 100);
 }
 
+const recordBtn = document.querySelector(".recordbtn");
+const playBtn = document.querySelector(".playBtn");
+const saveBtn = document.querySelector(".saveBtn");
 
-//hvjhkn
-/*
-nklnlkn
+recordBtn.addEventListener("click", toggleRecording);
+playBtn.addEventListener("click", playSong);
+saveBtn.addEventListener("click", saveSong);
 
-*/
+function toggleRecording() {
+  recordBtn.classList.toggle("active");
+
+  if (isRecording()) {
+    startRecording();
+    recordBtn.innerHTML = "Stop Recording";
+  } else {
+    stopRecording();
+    recordBtn.innerHTML = "Record";
+  }
+}
+
+function isRecording() {
+  return recordBtn != null && recordBtn.classList.contains("active");
+}
+
+function startRecording() {
+  recordingStartTime = Date.now();
+  songNotes = [];
+  playBtn.classList.remove("show");
+  saveBtn.classList.remove("show");
+}
+
+function stopRecording() {
+  playBtn.classList.add("show");
+  saveBtn.classList.add("show");
+}
+
+function playSong() {
+  if (songNotes.length === 0) return;
+  songNotes.forEach((note) => {
+    setTimeout(() => {
+      makeSound(note.key);
+    }, note.startTime);
+  });
+}
+
+function recordNote(note) {
+  songNotes.push({
+    key: note,
+    startTime: Date.now() - recordingStartTime,
+  });
+}
+
+function saveSong() {
+  if (songNotes) {
+    const serializedSongNotes = JSON.stringify(songNotes);
+    let saveAs = prompt("Save as : ");
+    localStorage.setItem(saveAs, serializedSongNotes);
+  }
+
+  saveBtn.classList.remove("show");
+  location.reload();
+}
+
+function retrieveSongNotes(nameOfSong) {
+  const serializedSongNotes = localStorage.getItem(nameOfSong);
+  if (serializedSongNotes) {
+    savedSongNotes = JSON.parse(serializedSongNotes);
+  }
+}
+function playSavedSong() {
+  if (savedSongNotes.length === 0) return;
+  savedSongNotes.forEach((note) => {
+    setTimeout(() => {
+      makeSound(note.key);
+    }, note.startTime);
+  });
+}
+
+// let playMySavedSong = document.querySelector(".playSavedSong");
+
+// playMySavedSong.addEventListener("click", () => {
+//   retrieveSongNotes();
+//   playSavedSong();
+// });
+
+function getAllLocalStorageKeys() {
+  const keys = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    keys.push(localStorage.key(i));
+  }
+  return keys;
+}
+
+function deleteSong(key) {
+  localStorage.removeItem(key);
+}
+
+function displayAllKeysInDiv() {
+  const keys = getAllLocalStorageKeys();
+  const mySongsDiv = document.querySelector(".mySongs");
+  const myNo = document.querySelector("#no");
+
+  keys.forEach(function (key, index) {
+    const songName = localStorage.getItem(key);
+    if (songName) {
+      myNo.classList.add("no");
+      const songContainer = document.createElement("div");
+      songContainer.classList.add("song-container");
+
+      const btnContainer = document.createElement("div");
+      songContainer.classList.add("btn-container");
+
+      const songElement = document.createElement("p");
+      songElement.textContent = `${key}`;
+
+      // Create a play button element
+      const playButton = document.createElement("button");
+      playButton.textContent = "Play";
+
+      playButton.addEventListener("click", function () {
+        // Play the respective song when the play button is clicked
+
+        retrieveSongNotes(key);
+        playSavedSong();
+      });
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+
+      deleteButton.addEventListener("click", function () {
+        // Handle delete functionality here
+        deleteSong(key);
+        // Refresh the list of songs after deleting
+        location.reload();
+      });
+
+      mySongsDiv.appendChild(songContainer);
+      // Append the play button and the song name to the song container
+      songContainer.appendChild(songElement);
+      songContainer.appendChild(btnContainer);
+      btnContainer.appendChild(playButton);
+      btnContainer.appendChild(deleteButton);
+      // Append the song container to the "mySongs" div
+    }
+  });
+}
